411431cf542fffa5e8797e0f44feff2a94a1779b
J.J. Oliphant <olimejj@gmail.com>
2024-09-30 13:34:17 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,11 @@
 #bubble(list) - > sorted ist
+def bubble(list):
+  return sorted(list)
 
 #selection(list) -> sorted list
+def selection(list):
+  return sorted(list)
 
 #insertion(list) > sorted list
+def insertion():
+  return sorted(list)
